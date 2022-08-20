8124e9e21c4070f6bb3743fd1f18eaae0365b761
naka-c1024 <ny42800824@gmail.com>
2022-08-20 16:52:08 +0900

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,15 +1,38 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack
+#include <stack>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack();
-	~MutantStack();
-	MutantStack(MutantStack const &other);
-	MutantStack &operator=(MutantStack const &other);
-private:
+	MutantStack() {}
+	~MutantStack() {}
+	MutantStack(MutantStack const &other) : std::stack<T>(other)
+	{
+		*this = other;
+	}
+	MutantStack &operator=(MutantStack const &other)
+	{
+		if (this != &other)
+		{
+			std::stack<T>::operator=(other);
+		}
+		return *this;
+	}
 
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator	begin()
+	{
+		return this->c.begin();
+	}
+	iterator	end()
+	{
+		return this->c.end();
+	}
 };
 
 #endif /* MUTANTSTACK_HPP */
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,41 @@
 #include "MutantStack.hpp"
+#include <list>
+
+void	list_test(void)
+{
+	std::cout << "\n---- compare MutantStack and std::list  ----" << std::endl;
+	MutantStack<int>	mu;
+	std::list<int>		li;
+	for (size_t i = 0; i < 10; i++)
+		mu.push(i);
+	for (size_t i = 0; i < 10; i++)
+		li.push_back(i);
+	MutantStack<int>::iterator	mit = mu.begin();
+	std::list<int>::iterator	lit = li.begin();
+	mit++;
+	lit++;
+	mit--;
+	lit--;
+	MutantStack<int>::iterator	m_end_it = mu.end();
+	std::list<int>::iterator	l_end_it = li.end();
+	std::cout << "MutantStack" << std::endl;
+	while (mit != m_end_it)
+	{
+		std::cout << *mit << " ";
+		mit++;
+	}
+	std::cout << "\nstd::list" << std::endl;
+	while (lit != l_end_it)
+	{
+		std::cout << *lit << " ";
+		lit++;
+	}
+	std::cout << std::endl;
+}
 
 int	main()
 {
+	std::cout << "---- subject tests ----" << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -30,5 +64,6 @@ int	main()
 	++it;
 	}
 	std::stack<int> s(mstack);
+	list_test();
 	return 0;
 }
